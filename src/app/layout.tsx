a754e5f6379f480dfae64251a72d95c8b963674c
SauceX <mahmoudzak4@gmail.com>
2024-03-20 02:22:04 +0300

    chore: add session provider=

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 
+import { SessionProvider } from "@/components/providers/session-provider";
 import { TRPCReactProvider } from "@/trpc/client";
-// import { SessionProvider } from "@/components/providers/session-provider";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -27,19 +27,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`font-sans ${inter.variable}`}>
-        {/* <SessionProvider> */}
-        <TRPCReactProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
-          <Toaster richColors duration={2700} />
-        </TRPCReactProvider>
-        {/* </SessionProvider> */}
+        <SessionProvider>
+          <TRPCReactProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {children}
+            </ThemeProvider>
+            <Toaster richColors duration={2700} />
+          </TRPCReactProvider>
+        </SessionProvider>
       </body>
     </html>
   );
