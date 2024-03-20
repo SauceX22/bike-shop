44ecb661a6ba0eb3ce49e7a72b30f40247f9af44
SauceX <mahmoudzak4@gmail.com>
2024-03-20 14:29:48 +0300

    fix: pass entire user obj

--- src/app/(reservations)/reservations/layout.tsx
+++ src/app/(reservations)/reservations/layout.tsx
@@ -24,13 +24,7 @@ export default async function DashboardLayout({
       <header className="sticky top-0 z-40 border-b bg-background">
         <div className="container flex h-16 items-center justify-between py-4">
           <MainNav items={dashboardConfig.mainNav} />
-          <UserAccountNav
-            user={{
-              name: session?.user.name,
-              image: session?.user.image,
-              email: session?.user.email,
-            }}
-          />
+          <UserAccountNav user={session.user} />
         </div>
       </header>
       <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
