3c3df96599993c7af2f1cbbe85109ea6c3b97eb5
SauceX <mahmoudzak4@gmail.com>
2024-03-20 14:29:20 +0300

    feat: add user avatar

--- src/components/user-avatar.tsx
+++ src/components/user-avatar.tsx
@@ -1,14 +1,32 @@
 import { type User } from "@prisma/client";
 import { type AvatarProps } from "@radix-ui/react-avatar";
 
-import { Icons } from "@/components/icons";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { getInitials } from "@/lib/utils";
+import Image from "next/image";
+import { useMemo } from "react";
 
 interface UserAvatarProps extends AvatarProps {
   user: Pick<User, "image" | "name">;
 }
 
 export function UserAvatar({ user, ...props }: UserAvatarProps) {
+  const image = useMemo(() => {
+    const defaultImage = "https://avatar.vercel.sh/rauchg?size=40";
+
+    if (!user.name?.split(" ")[0]) return defaultImage;
+
+    const fname = user.name.split(" ")[0];
+    const initials = getInitials(user.name);
+    const firstName = fname
+      ? fname.charAt(0).toUpperCase() + fname.slice(1)
+      : null;
+
+    return firstName
+      ? `https://avatar.vercel.sh/${firstName}.svg?size=40&text=${initials}`
+      : defaultImage;
+  }, [user.name]);
+
   return (
     <Avatar {...props}>
       {user.image ? (
@@ -16,7 +34,14 @@ export function UserAvatar({ user, ...props }: UserAvatarProps) {
       ) : (
         <AvatarFallback>
           <span className="sr-only">{user.name}</span>
-          <Icons.user className="h-4 w-4" />
+          {/* <Icons.user className="h-4 w-4" /> */}
+          <Image
+            // would be "https://avatar.vercel.sh/Mahmoud?size=40&text=MA"
+            src={image}
+            alt="Picture"
+            width={40}
+            height={40}
+          />
         </AvatarFallback>
       )}
     </Avatar>
