950ea93fe138c4a1726699ef91f6320ebc988611
SauceX <mahmoudzak4@gmail.com>
2024-03-21 23:55:52 +0300

    feat: add star riting with debounce for perf

--- src/components/reservation-item.tsx
+++ src/components/reservation-item.tsx
@@ -18,15 +18,18 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { RatingGroup } from "@/components/ui/rating";
 import { Separator } from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
 import { cn } from "@/lib/utils";
+import { ratingSchema } from "@/lib/validations/general";
 import { api } from "@/trpc/client";
 import { type Bike, type Reservation } from "@prisma/client";
 import { format, isSameYear } from "date-fns";
+import { debounce } from "lodash"; // Ensure lodash's debounce is correctly imported
 import { MapPin } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { toast } from "sonner";
 
 interface ReservationItemProps {
@@ -38,6 +41,35 @@ export function ReservationItem({ reservation, bike }: ReservationItemProps) {
   const [showCancelDialog, setShowCancelDialog] = useState(false);
   const router = useRouter();
   const apiUtils = api.useUtils();
+  // debounce the rating change to avoid spamming the server
+  const { mutateAsync: rateBike } = api.reservation.rateBike.useMutation({
+    onError(err) {
+      toast.error("Something went wrong.", {
+        description: err.message,
+      });
+    },
+    async onSuccess(rating, variables, context) {
+      toast.success("Rating submitted", {
+        description: `Rating for *${bike.name}* from ${format(
+          reservation.startDate,
+          "d MMM yyyy",
+        )} to ${format(reservation.endDate, "d MMM yyyy")} submitted successfully.`,
+      });
+      await apiUtils.reservation.invalidate();
+      router.refresh();
+    },
+  });
+
+  // Create the debounced function with useCallback to ensure it does not get recreated on every render
+  const debouncedRateBike = debounce(async (value: string) => {
+    const result = ratingSchema.safeParse(value);
+    if (result.success) {
+      await rateBike({
+        id: reservation.id,
+        rating: result.data,
+      });
+    }
+  }, 1000); // Debounce time is 1500ms
 
   const { mutateAsync: cancelReservation } =
     api.reservation.cancelReservation.useMutation({
@@ -59,19 +91,21 @@ export function ReservationItem({ reservation, bike }: ReservationItemProps) {
       },
     });
 
-  const differentYear = !isSameYear(
-    reservation.startDate ?? new Date(),
-    reservation.endDate ?? new Date(),
-  );
+  const differentYear = useMemo(() => {
+    return !isSameYear(
+      reservation.startDate ?? new Date(),
+      reservation.endDate ?? new Date(),
+    );
+  }, [reservation.startDate, reservation.endDate]);
 
   return (
     <>
-      <Card className="h-[22rem] flex flex-col">
+      <Card className="h-[25rem] flex flex-col">
         <CardHeader>
           <CardTitle>{bike.name}</CardTitle>
           <CardDescription>{bike.model}</CardDescription>
         </CardHeader>
-        <CardContent className="flex flex-col items-stretch justify-center gap-4">
+        <CardContent className="flex flex-col items-stretch justify-center gap-4 py-0">
           <div className="flex flex-row items-center justify-between mb-auto px-6 py-2">
             <span className="text-secondary-foreground flex-shrink-0 flex gap-2 w-fit">
               <MapPin className="w-6 h-6" />
@@ -84,7 +118,7 @@ export function ReservationItem({ reservation, bike }: ReservationItemProps) {
           </div>
           <Card>
             {/* make the column in the middle the smallest */}
-            <CardContent className="grid grid-cols-3 grid-rows-1 p-4 gap-4 ">
+            <CardContent className="grid grid-cols-3 grid-rows-1 px-4 py-2 gap-4">
               <div className="mx-auto">
                 Start Date
                 <p className="font-semibold text-xl">
@@ -110,6 +144,24 @@ export function ReservationItem({ reservation, bike }: ReservationItemProps) {
               </div>
             </CardContent>
           </Card>
+          {reservation.rating ? (
+            <p className="text-center">
+              You rated this bike {reservation.rating} stars
+            </p>
+          ) : (
+            <p className="text-center text-destructive">
+              This bike has not been rated yet
+            </p>
+          )}
+          <RatingGroup
+            ratingSteps={5}
+            className="mx-auto"
+            defaultValue={reservation.rating?.toString() ?? "0"}
+            value={reservation.rating?.toString() ?? "0"}
+            onValueChange={async (value) => {
+              await debouncedRateBike(value);
+            }}
+          />
         </CardContent>
         <CardFooter className="mt-auto">
           <Button
@@ -117,7 +169,7 @@ export function ReservationItem({ reservation, bike }: ReservationItemProps) {
             className="w-full"
             onClick={() => setShowCancelDialog(true)}
           >
-            Cancel Reservation
+            Cancel
           </Button>
         </CardFooter>
       </Card>
@@ -126,10 +178,11 @@ export function ReservationItem({ reservation, bike }: ReservationItemProps) {
           <AlertDialogHeader>
             <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
             <AlertDialogDescription>
-              This action cannot be undone. This will permanently cancel
-              {bike.name}&apos;s reservation for{" "}
-              {format(reservation.startDate, "d MMM yyyy")} to{" "}
-              {format(reservation.endDate, "d MMM yyyy")}.
+              This action cannot be undone. This will permanently cancel{" "}
+              <b>{bike.name}&apos;s</b>
+              reservation for
+              <b>{format(reservation.startDate, "d MMM yyyy")}</b>
+              to <b>{format(reservation.endDate, "d MMM yyyy")}.</b>
             </AlertDialogDescription>
           </AlertDialogHeader>
           <AlertDialogFooter>
