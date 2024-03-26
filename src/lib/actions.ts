829abcffd61bcaa8f6adf73cb65be635710fc47e
SauceX <mahmoudzak4@gmail.com>
2024-03-26 21:25:58 +0300

    chore: some inline docs

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -8,6 +8,15 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
 
+/**
+ * Search server action to redirect to the callback url with the search params
+ *
+ * read more about server actions here: https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions-and-mutations
+ *
+ * @param data The form data to be submitted
+ * @param callbackUrl The url to redirect to after the search
+ * @returns void
+ */
 export async function searchAction(
   data: z.infer<typeof filterFormSchema>,
   callbackUrl: string,
@@ -28,10 +37,23 @@ export async function searchAction(
   return redirect(url);
 }
 
+/**
+ * Server action to revalidate the cache of a path after a relevant mutation
+ *
+ * @param path path to revalidate the cache of
+ * @returns void
+ */
 export async function revalidatePathCache(path: string) {
   return revalidatePath(path);
 }
 
+/**
+ * Get filtered bikes based on the search parameters
+ *
+ * @param searchParams Search parameters used to filter the bikes
+ * @param availableOnly Whether to only return available bikes
+ * @returns The filtered bikes after query
+ */
 export const getFilteredBikes = async (
   searchParams: {
     query?: string;
