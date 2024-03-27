0db91206b72434d7bad1114b4e53529a2543f802
SauceX <mahmoudzak4@gmail.com>
2024-03-27 06:40:15 +0300

    feat: bike icon

--- src/app/(auth)/auth/register/page.tsx
+++ src/app/(auth)/auth/register/page.tsx
@@ -34,7 +34,7 @@ export default async function RegisterPage() {
       <div className="lg:p-8">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
-            <Icons.logo className="mx-auto h-6 w-6" />
+            <Icons.logo className="mx-auto h-16 w-16" />
             <h1 className="text-2xl font-semibold tracking-tight">
               Create an account
             </h1>
